1847bcea4e0dba8df5002ca31edff2edc1878f3e
5161362 <5161362@liae.edu.uniube.br>
2024-08-19 20:28:11 -0300

    add login

--- index.php
+++ index.php
@@ -15,7 +15,9 @@
     </head>
     <body>
         <form method="post" action="login.php" onSubmit="return valida();">
+            <label for="cpf">CPF: </label>
             <input type="text" name="cpf" id="cpf">
+            <label for="senha">Senha: </label>
             <input type="text" name="senha" id="senha">
             <input type="submit" value="Enviar">
         </form>
