5397547b9dfaaccaf0c8c4283155b8ffb9917fe5
5161362 <5161362@liae.edu.uniube.br>
2024-08-19 19:55:06 -0300

    add form

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-    echo "teste";
-?>
+<html>
+    <title>Aula PHP</title>
+    <head>
+        <script>
+            function valida() {
+                nome = document.getElementById("nome").value;
+                if (nome == "") {
+                    alert ("Digite um nome");
+                    document.getElementById("nome").focus();
+                    return false;
+                }
+                return true;
+            }
+        </script>
+    </head>
+    <body>
+        <form method="post" action="login.php" onSubmit="return valida();">
+            <input type="text" name="cpf" id="cpf">
+            <input type="text" name="senha" id="senha">
+        </form>
+    </body>
+</html>
