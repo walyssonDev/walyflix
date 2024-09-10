d4d1be3e0965072ad4874d06d6c7ab2071cdbdb8
oCrazyoff <walyssonribeiro3@gmail.com>
2024-09-09 21:27:39 -0300

    update cadastro

--- cadastrar.php
+++ cadastrar.php
@@ -1,13 +1,12 @@
 <?php
 include("conexao.php");
-include("valida.php");
 
 $cpf = $_POST["cpf"];
 $nome = $_POST["nome"];
 $senha = $_POST["senha"];
 
-$sql = "INSERT INTO  'usuarios' ('cpf', 'nome', 'senha') VALUES ('$cpf', '$nome', '$senha')";
+$sql = ("INSERT INTO `usuarios` (`cpf`, `nome`, `senha`) VALUES ('$cpf', '$nome', '$senha')");
 $resultado = $conn->query($sql);
 
-header("Location: cadastro.php");
+header("Location: cadastrar.php");
 ?>
