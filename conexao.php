ec12c46358ebeca32b3a04c896c4f320036a6289
oCrazyoff <walyssonribeiro3@gmail.com>
2024-09-18 10:02:30 -0300

    update bd

--- conexao.php
+++ conexao.php
@@ -1,13 +1,11 @@
 <?php
 
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$dbname = "cadastro_filmes";
+$servidor = "sql305.infinityfree.com";
+$usuario = "if0_37334546";
+$senha = "8zVebj7zkBN";
+$dbname = "if0_37334546_cadastro_filmes";
 
 $conn = new mysqli($servidor, $usuario, $senha, $dbname);
-if($conn->connect_error) {
-    die("Falha na conexão".$conn->connect_error);
+if ($conn->connect_error) {
+    die("Falha na conexão" . $conn->connect_error);
 }
-
-?>
