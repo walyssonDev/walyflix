7eacbbe66cc2516ac30cb6d2bb2e343f9d70c84f
oCrazyoff <walyssonribeiro3@gmail.com>
2024-09-26 08:40:28 -0300

    update txt

--- filmes.php
+++ filmes.php
@@ -6,30 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filmes</title>
     <style>
-        .interface{
-            display: flex;
-            flex-wrap: wrap;
-            gap: 1em;
-        }
-        .filme{
-            background-color: #023047;
-            width: 25%;
-            height: 20em;
-            color: white;
-            border-radius: .5em;
-            overflow: hidden;
-            box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-            cursor: pointer;
-        }
+    .interface {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1em;
+    }
 
-        .filme img{
-            width: 100%;
-            height: 10em;
-        }
+    .filme {
+        background-color: #023047;
+        width: 25%;
+        height: 20em;
+        color: white;
+        border-radius: .5em;
+        overflow: hidden;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+        cursor: pointer;
+    }
 
-        .filme p{
-            padding: 0 1em;
-        }
+    .filme img {
+        width: 100%;
+        height: 10em;
+    }
+
+    .filme p {
+        padding: 0 1em;
+    }
     </style>
 </head>
 
@@ -37,7 +38,10 @@
     <div class="interface">
         <article class="filme">
             <img src="https://eivfina4wt8.exactdn.com/wp-content/uploads/2023/11/12112433/divertidamente-2-capa.jpg">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos distinctio voluptas laboriosam similique ex natus laborum suscipit, unde ipsum dolorem itaque voluptates quaerat facilis sunt praesentium veritatis deleniti eum ea?</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos distinctio voluptas laboriosam
+                similique ex natus laborum suscipit.
+            </p>
         </article>
     </div>
 </body>
