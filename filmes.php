c9428eba718d97e0b41e8d695f3cdeabeb3a23b8
Walysson <walysson@localhost.localdomain>
2024-09-26 20:51:11 -0300

    fix wolverine fake

--- filmes.php
+++ filmes.php
@@ -11,46 +11,46 @@ include("conexao.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filmes</title>
     <style>
-    body * {
-        box-sizing: border-box;
-        padding: 0;
-        margin: 0;
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    }
+        body * {
+            box-sizing: border-box;
+            padding: 0;
+            margin: 0;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
 
-    .interface {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 1em;
-    }
+        .interface {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 1em;
+        }
 
-    .filme {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        background-color: #023047;
-        color: white;
-        border-radius: .5em;
-        box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
-        cursor: pointer;
-    }
+        .filme {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            background-color: #023047;
+            color: white;
+            border-radius: .5em;
+            box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+            cursor: pointer;
+        }
 
-    .filme img {
-        width: 100%;
-        height: 10em;
-        border-radius: .4em .4em 0 0;
-    }
+        .filme img {
+            width: 100%;
+            height: 10em;
+            border-radius: .4em .4em 0 0;
+        }
 
-    .filme .txt-filme {
-        text-align: center;
-        font-size: 20px;
-        padding: .5em 1em;
-    }
+        .filme .txt-filme {
+            text-align: center;
+            font-size: 20px;
+            padding: .5em 1em;
+        }
 
-    .filme .txt-filme p {
-        margin: 0;
-    }
+        .filme .txt-filme p {
+            margin: 0;
+        }
     </style>
 </head>
 
@@ -71,7 +71,7 @@ include("conexao.php");
             ";
         }
         ?>
-        <a href="https://www.xvideos.com/">
+        <a target="_blank" href="https://www.youtube.com/watch?v=WHJRwLJrSGM">
             <article class="filme">
                 <img
                     src="https://img14.tokyvideo.com/videos/277/277145/previews/previews_0012_custom_1687197636.7012.jpg">
