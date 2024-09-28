e792bc6902b4296308432692ed464767196bbb4b
Walysson <walysson@localhost.localdomain>
2024-09-27 21:35:57 -0300

    fix title

--- principal.php
+++ principal.php
@@ -9,7 +9,7 @@ include("valida.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <title>Document</title>
+    <title>Principal - Filmes</title>
     <style>
         body {
             box-sizing: border-box;
