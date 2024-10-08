f04ed638bee19c9e06e6c1b95332e1b4ea4d2595
Walysson <walysson@localhost.localdomain>
2024-10-08 16:01:13 -0300

    fix favoritos

--- favoritar.php
+++ favoritar.php
@@ -12,7 +12,13 @@ if ($resultado->num_rows == 0) {
     $sql = "INSERT INTO favoritos (cpf, filme_id) VALUES ('$cpf', '$id')";
     $conn->query($sql);
 } else {
-    $sql = "DELETE FROM favoritos WHERE cpf = '$cpf' AND filme_id = '$id'";
+    $sqlFav = "SELECT id FROM favoritos WHERE cpf = '$cpf' AND filme_id = '$id'";
+    $resultado = $conn->query($sqlFav);
+    $row = $resultado->fetch_assoc();
+
+    $idFav = $row['id'];
+
+    $sql = "DELETE FROM favoritos WHERE id = '$idFav'";
     $conn->query($sql);
 }
 
