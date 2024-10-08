5c01af0f068901a4b3c7e8169fe861f3e8305ba6
Walysson <walysson@localhost.localdomain>
2024-10-08 11:23:15 -0300

    update favoritar filmes

--- favoritar.php
+++ favoritar.php
@@ -5,7 +5,15 @@ include("valida.php");
 $id = $_POST['id'];
 $cpf = $_SESSION['cpf'];
 
-$sql = "UPDATE `usuarios` SET `favoritos` = '$id' WHERE `usuarios`.`cpf` = '$cpf'";
-$resultado = $conn->query($sql);
+$sqlVerificar = "SELECT * FROM favoritos WHERE cpf = '$cpf' AND filme_id = '$id'";
+$resultado = $conn->query($sqlVerificar);
+
+if ($resultado->num_rows == 0) {
+    $sql = "INSERT INTO favoritos (cpf, filme_id) VALUES ('$cpf', '$id')";
+    $conn->query($sql);
+} else {
+    $sql = "DELETE FROM favoritos WHERE cpf = '$cpf' AND filme_id = '$id'";
+    $conn->query($sql);
+}
 
 header("Location: filmes.php");
--- favoritos.php
+++ favoritos.php
@@ -1,3 +1,9 @@
+<?php
+include("conexao.php");
+include("valida.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,18 +11,132 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
-        body {
+        body * {
             box-sizing: border-box;
             padding: 0;
             margin: 0;
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         }
+
+        .interface {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 1em;
+        }
+
+        .interface a {
+            text-decoration: none;
+            color: white;
+        }
+
+        .filme {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            background-color: #023047;
+            color: white;
+            border-radius: .5em;
+            box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+            cursor: pointer;
+            position: relative;
+            overflow: hidden;
+        }
+
+        .fav {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: absolute;
+            top: 0;
+            right: 0;
+            padding: 1%;
+            border-radius: 0 0 0 50%;
+            backdrop-filter: blur(100px);
+        }
+
+        .fav i {
+
+            color: #FFD100;
+            font-size: 30px;
+        }
+
+        .filme img {
+            width: 100%;
+            height: 10em;
+            border-radius: .4em .4em 0 0;
+        }
+
+        .filme .txt-filme {
+            text-align: center;
+            font-size: 20px;
+            padding: .5em 1em;
+        }
+
+        .filme .txt-filme p {
+            margin: 0;
+        }
+
+        input[type="submit"] {
+            padding: .7em 1em;
+            border-radius: .5em;
+            border: none;
+            background-color: #bf0603;
+            color: white;
+            cursor: pointer;
+            margin-top: 1em;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #780000;
+        }
     </style>
 </head>
 
 <body>
-    <h1>FILMES FAVORITOS</h1>
+    <div class="interface">
+        <?php
+        $cpf = $_SESSION['cpf'];
+
+        $sql = "SELECT * FROM favoritos WHERE cpf = '$cpf'";
+        $resultado = $conn->query($sql);
+
+        while ($row = $resultado->fetch_assoc()) {
+            $id = $row['filme_id'];
+            $sqlFilme = "SELECT * FROM filmes WHERE id = '$id'";
+            $resultadoFilme = $conn->query($sqlFilme);
+
+            $rowFilme = $resultadoFilme->fetch_assoc();
+
+            echo "<a href='assistir_filme.php?id=" . $rowFilme['id'] . "'>";
+            echo "
+                <article class='filme'>";
+            echo "<div class='fav'>
+                        <i id='nfav' class='bi bi-star'></i>
+                        </div>";
+            echo "
+                <img src='" . $rowFilme['path'] . "'>
+                <div class='txt-filme'>
+                    <p>" . $rowFilme['nome'] . "</p>
+                </div>
+            </article>
+                ";
+            echo "</a>";
+            echo "
+            <div class='favt'>
+            <form method='post' action='favoritar.php'>
+                <input type='hidden' name='id' value='" . $row['id'] . "'>
+                <button type='submit'>
+                    <i id='nfav' class='bi bi-star'></i>
+                </button>
+            </form>
+        </div>
+        ";
+        }
+        ?>
+    </div>
 </body>
 
 </html>
