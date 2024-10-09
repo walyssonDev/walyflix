083ca51850b2438ff2bf582238fb6ddf52db2d2d
oCrazyoff <walyssonribeiro3@gmail.com>
2024-10-09 19:28:17 -0300

    fix idention

--- filmes.php
+++ filmes.php
@@ -10,89 +10,90 @@
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Filmes</title>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+            <link rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
             <style>
-                body * {
-                    box-sizing: border-box;
-                    padding: 0;
-                    margin: 0;
-                    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-                }
+            body * {
+                box-sizing: border-box;
+                padding: 0;
+                margin: 0;
+                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            }
 
-                .interface {
-                    display: flex;
-                    flex-wrap: wrap;
-                    gap: 1em;
-                }
+            .interface {
+                display: flex;
+                flex-wrap: wrap;
+                gap: 1em;
+            }
 
-                .interface a {
-                    text-decoration: none;
-                    color: white;
-                }
+            .interface a {
+                text-decoration: none;
+                color: white;
+            }
 
-                .filme {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    flex-direction: column;
-                    background-color: #023047;
-                    color: white;
-                    border-radius: .5em;
-                    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
-                    cursor: pointer;
-                    position: relative;
-                    overflow: hidden;
-                }
+            .filme {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+                background-color: #023047;
+                color: white;
+                border-radius: .5em;
+                box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+                cursor: pointer;
+                position: relative;
+                overflow: hidden;
+            }
 
-                .fav button {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    position: absolute;
-                    top: 0;
-                    right: 0;
-                    padding: 1%;
-                    border: none;
-                    border-radius: 0 0 0 50%;
-                    background-color: black;
-                    cursor: pointer;
-                }
+            .fav button {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                top: 0;
+                right: 0;
+                padding: 1%;
+                border: none;
+                border-radius: 0 0 0 50%;
+                background-color: black;
+                cursor: pointer;
+            }
 
-                .fav i {
+            .fav i {
 
-                    color: #FFD100;
-                    font-size: 30px;
-                }
+                color: #FFD100;
+                font-size: 30px;
+            }
 
-                .filme img {
-                    width: 100%;
-                    height: 10em;
-                    border-radius: .4em .4em 0 0;
-                }
+            .filme img {
+                width: 100%;
+                height: 10em;
+                border-radius: .4em .4em 0 0;
+            }
 
-                .filme .txt-filme {
-                    text-align: center;
-                    font-size: 20px;
-                    padding: .5em 1em;
-                }
+            .filme .txt-filme {
+                text-align: center;
+                font-size: 20px;
+                padding: .5em 1em;
+            }
 
-                .filme .txt-filme p {
-                    margin: 0;
-                }
+            .filme .txt-filme p {
+                margin: 0;
+            }
 
-                input[type="submit"] {
-                    padding: .7em 1em;
-                    border-radius: .5em;
-                    border: none;
-                    background-color: #bf0603;
-                    color: white;
-                    cursor: pointer;
-                    margin-top: 1em;
-                }
+            input[type="submit"] {
+                padding: .7em 1em;
+                border-radius: .5em;
+                border: none;
+                background-color: #bf0603;
+                color: white;
+                cursor: pointer;
+                margin-top: 1em;
+            }
 
-                input[type="submit"]:hover {
-                    background-color: #780000;
-                }
+            input[type="submit"]:hover {
+                background-color: #780000;
+            }
             </style>
         </head>
 
@@ -121,7 +122,7 @@
                     <div class='txt-filme'>
                         <p>" . $row['nome'] . "</p>
                     </div>
-                </article>
+                    </article>
                     ";
                     echo "</a>";
                 }
