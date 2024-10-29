0b5900538115f53218c4e4d6c31dc92ac52e03a6
oCrazyoff <walyssonribeiro3@gmail.com>
2024-10-29 13:22:37 -0300

    fix nome vazio

--- pages/assistir_filme.php
+++ pages/assistir_filme.php
@@ -28,14 +28,14 @@ $extensao = pathinfo($link_limpo, PATHINFO_EXTENSION);
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-VX1YBC3426"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
+        window.dataLayer = window.dataLayer || [];
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-    gtag('config', 'G-VX1YBC3426');
+        gtag('config', 'G-VX1YBC3426');
     </script>
 </head>
 
@@ -63,6 +63,10 @@ $extensao = pathinfo($link_limpo, PATHINFO_EXTENSION);
                     $stmt->bind_result($nome, $imgData);
                     $stmt->fetch();
                     $stmt->close();
+
+                    if (empty($nome)) {
+                        $nome = "Anonimo";
+                    }
 
                     if (!empty($imgData)) {
                         $imgSrc = 'data:image/jpeg;base64,' . base64_encode($imgData);
@@ -127,7 +131,7 @@ $extensao = pathinfo($link_limpo, PATHINFO_EXTENSION);
         </div>
     </div>
     <script>
-    <?php
+        <?php
         if (isset($_GET['resposta'])) {
             echo "alert('" . $_GET['resposta'] . "')";
         }
