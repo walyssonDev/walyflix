53358319c3d31219e0e67f67f2e850c22a8f33fb
oCrazyoff <walyssonribeiro3@gmail.com>
2024-10-31 10:59:15 -0300

    fix headers

--- pages/assistir_filme.php
+++ pages/assistir_filme.php
@@ -1,10 +1,6 @@
 <?php
 include("../admin/conexao.php");
 include("../action/valida.php");
-header("Cache-Control: no-cache, no-store, must-revalidate");
-header("Pragma: no-cache");
-header("Expires: 0");
-
 
 $id = $_GET['id'];
 
