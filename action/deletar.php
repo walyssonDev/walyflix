ab969f19f9d37933984946e9a6677f5c146cccf6
oCrazyoff <walyssonribeiro3@gmail.com>
2024-11-05 13:31:32 -0300

    rework css filmes

--- action/deletar.php
+++ action/deletar.php
@@ -3,12 +3,6 @@ include("../admin/conexao.php");
 
 $cpf = $_POST["cpf"];
 
-$sqlUsuario = ("DELETE FROM usuarios WHERE cpf = ?");
-$stmt = $conn->prepare($sqlUsuario);
-$stmt->bind_param("s", $cpf);
-$stmt->execute();
-$stmt->close();
-
 $sqlFavoritos = "DELETE FROM favoritos WHERE cpf = ?";
 $stmt = $conn->prepare($sqlFavoritos);
 $stmt->bind_param("s", $cpf);
@@ -17,6 +11,12 @@ $stmt->close();
 
 $sqlComentarios = "DELETE FROM comentarios WHERE cpf = ?";
 $stmt = $conn->prepare($sqlComentarios);
+$stmt->bind_param("s", $cpf);
+$stmt->execute();
+$stmt->close();
+
+$sqlUsuario = ("DELETE FROM usuarios WHERE cpf = ?");
+$stmt = $conn->prepare($sqlUsuario);
 $stmt->bind_param("s", $cpf);
 $stmt->execute();
 $stmt->close();
