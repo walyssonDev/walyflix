9271b1848a931c87de0175649cb24e2124ea3247
oCrazyoff <walyssonribeiro3@gmail.com>
2025-02-01 09:26:58 -0300

    Refactor unzip.php to simplify extraction by using extractTo method for direct file extraction

--- pages/unzip.php
+++ pages/unzip.php
@@ -15,32 +15,10 @@ if (!is_dir($extractToPath)) {
 $zip = new ZipArchive;
 $res = $zip->open($zipFilePath);
 if ($res === TRUE) {
-    for ($i = 0; $i < $zip->numFiles; $i++) {
-        $filePath = $zip->getNameIndex($i);
-
-        // Corrige barras invertidas para garantir a estrutura correta
-        $filePath = str_replace('\\', '/', $filePath);
-
-        $fullPath = $extractToPath . '/' . $filePath;
-
-        // Se for um diretório, cria ele antes de extrair os arquivos
-        if (substr($filePath, -1) === '/') {
-            if (!is_dir($fullPath)) {
-                mkdir($fullPath, 0755, true);
-            }
-        } else {
-            // Criar diretório pai, se não existir
-            $dir = dirname($fullPath);
-            if (!is_dir($dir)) {
-                mkdir($dir, 0755, true);
-            }
-
-            // Extrair arquivo corretamente
-            copy("zip://$zipFilePath#$filePath", $fullPath);
-        }
-    }
-
+    // Extraindo tudo diretamente para garantir que os arquivos sejam extraídos corretamente
+    $zip->extractTo($extractToPath);
     $zip->close();
+
     echo 'Descompactação concluída com sucesso!';
 } else {
     echo 'Falha ao descompactar o arquivo. Código de erro: ' . $res;
