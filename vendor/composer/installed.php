f9a26c8f6c98dcd49a82a99c032465aa3b9d15bc
oCrazyoff <walyssonribeiro3@gmail.com>
2025-02-01 10:20:07 -0300

    Update subproject references in installed.php and result-type

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '229f021e0d0c641c3bdd2b0879e481eabd789a16',
+        'reference' => 'd57f165d9be84beecc946c1a25d65cc94c440a3f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '229f021e0d0c641c3bdd2b0879e481eabd789a16',
+            'reference' => 'd57f165d9be84beecc946c1a25d65cc94c440a3f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
