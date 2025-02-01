4f8b4f2004cd8704c593df4cf657e9ca8c8585fc
oCrazyoff <walyssonribeiro3@gmail.com>
2025-01-31 22:44:24 -0300

    Add debugging output for environment variables in conexao.php

--- handler/utils/conexao.php
+++ handler/utils/conexao.php
@@ -6,6 +6,9 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(dirname(__DIR__, 2), 'senha.env');
 $dotenv->load();
 
+// Debugging: Verificar se as variáveis de ambiente foram carregadas
+var_dump($_ENV);
+
 if (!isset($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'])) {
     die('Erro: Variáveis de ambiente não carregadas corretamente.');
 }
@@ -20,7 +23,15 @@ $usuario = $_ENV['DB_USER'];
 $senha = $_ENV['DB_PASS'];
 $dbname = $_ENV['DB_NAME'];
 
+// Debugging: Exibir os valores das variáveis de ambiente
+echo "DB_HOST: $servidor\n";
+echo "DB_USER: $usuario\n";
+echo "DB_PASS: $senha\n";
+echo "DB_NAME: $dbname\n";
+
 $conn = new mysqli($servidor, $usuario, $senha, $dbname);
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
+
+// Código adicional para manipulação do banco de dados pode ser adicionado aqui
