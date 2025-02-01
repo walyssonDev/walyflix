4611a10cbe36276f39a90ba0bd2662a3bade62c6
oCrazyoff <walyssonribeiro3@gmail.com>
2025-02-01 10:28:25 -0300

    Remove sensitive environment variables from .gitignore and add new environment configuration file; mark subproject commits as dirty

--- handler/utils/conexao.php
+++ handler/utils/conexao.php
@@ -10,9 +10,6 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(dirname(__DIR__, 2), 'senha.env');
 $dotenv->load();
 
-// Debugging: Verificar se as variáveis de ambiente foram carregadas
-var_dump($_ENV);
-
 if (!isset($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'])) {
     die('Erro: Variáveis de ambiente não carregadas corretamente.');
 }
@@ -26,12 +23,6 @@ $servidor = $_ENV['DB_HOST'];
 $usuario = $_ENV['DB_USER'];
 $senha = $_ENV['DB_PASS'];
 $dbname = $_ENV['DB_NAME'];
-
-// Debugging: Exibir os valores das variáveis de ambiente
-echo "DB_HOST: $servidor\n";
-echo "DB_USER: $usuario\n";
-echo "DB_PASS: $senha\n";
-echo "DB_NAME: $dbname\n";
 
 $conn = new mysqli($servidor, $usuario, $senha, $dbname);
 if ($conn->connect_error) {
