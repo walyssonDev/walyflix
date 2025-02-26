d2a9bf79ae7458a1707c85c3ddb043cce9d3421d
oCrazyoff <walyssonribeiro3@gmail.com>
2025-02-26 10:46:52 -0300

    Improve loader styles for better visibility and adjust image size

--- includes/load.php
+++ includes/load.php
@@ -1,22 +1,22 @@
 <style>
-    .loader {
-        position: fixed;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 9999;
-        background-color: rgb(0, 0, 0);
-        opacity: .8;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+.loader {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    background-color: rgb(0, 0, 0);
+    opacity: 0.95;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 
-    .loader img {
-        width: 100px;
-        height: 100px;
-    }
+.loader img {
+    width: 40px;
+    height: 40px;
+}
 </style>
 
 <div id="loader" class="loader">
@@ -24,10 +24,10 @@
 </div>
 
 <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        var loader = document.getElementById('loader');
-        window.addEventListener('load', function() {
-            loader.style.display = 'none';
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    var loader = document.getElementById('loader');
+    window.addEventListener('load', function() {
+        loader.style.display = 'none';
     });
+});
 </script>
